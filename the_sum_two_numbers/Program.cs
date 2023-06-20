c5fecfa7f7ae53379fb2274a977cb3b987713875
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-20 10:09:50 +0200

    Aggiungere i file di progetto.

---
+++ the_sum_two_numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("insert the prime number= ");
+int number1= int.Parse(Console.ReadLine());
+Console.Write("insert the second number= ");
+int number2 = int.Parse(Console.ReadLine());
+int result= number1 + number2;
+Console.WriteLine("the result is= " + result);
+
+
+
+
